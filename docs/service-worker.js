3fb6f81d0e278948da99fceaac82420906b81f59
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2019-07-19 19:08:06 -0600

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -10,7 +10,7 @@
  * and re-run your build process.
  * See https://goo.gl/2aRDsh
  */
-
+ /*
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
@@ -24,12 +24,14 @@ self.addEventListener('message', (event) => {
 });
 
 workbox.core.clientsClaim();
+*/
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
+/*
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
@@ -37,3 +39,4 @@ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/i
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
+*/
